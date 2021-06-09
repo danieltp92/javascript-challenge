0347f5228e85f1388fa26003a51612f82d9035e0
danieltp92 <daniel.tp92@gmail.com>
2021-06-09 13:33:08 -0500

    update function for js

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 var tableBody = d3.select("tbody");
 // YOUR CODE HERE!
+function createTable(data){
+    console.log(data);
+    tableBody.html("");
+    data.forEach((element) => {
+        var row = tableBody.append("tr");
+        Object.values(element).forEach((values)=>{
+            var tcell = row.append("td");
+            tcell.text(values);
+        });
+    });
+}
