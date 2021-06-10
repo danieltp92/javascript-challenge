7638126bd9573eff299b0c5930bc488ea5cec4e0
danieltp92 <daniel.tp92@gmail.com>
2021-06-09 19:40:29 -0500

    make button works for filter data

--- UFO-level-1/static/js/alien_app.js
+++ UFO-level-1/static/js/alien_app.js
@@ -1,16 +1,36 @@
 // from data.js
+var newData = data;
 var tbody = d3.select("tbody");
 // YOUR CODE HERE!
+function createTable(data){
 
-console.log(data);
+    console.log(data);
 
-data.forEach(function(elements) {
-    console.log(elements);  
-    var line = tbody.append("tr");
-    
-    Object.entries(elements).forEach(function([key, value]) {
-        console.log(key, value);
-        var field = line.append("td");
-        field.text(value);
+    data.forEach(function(elements) {
+        console.log(elements);  
+        var line = tbody.append("tr");
+        
+        Object.entries(elements).forEach(function([key, value]) {
+            console.log(key, value);
+            var field = line.append("td");
+            field.text(value);
+        });
     });
-});
+}
+
+function filterButton(){
+
+    d3.event.preventDefault();
+
+    var cleanData = newData;
+    var fecha = d3.select("#datetime").property("value");
+    if(fecha){
+        cleanData = cleanData.filter(x => x.datetime === fecha);
+    }
+
+    createTable(cleanData);
+}
+
+d3.selectAll("#filter-btn").on("click",filterButton);
+
+createTable(newData);
